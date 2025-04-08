3ba1405af8d305a391f1b07df0a97e77c231e9a8
Isuri200060 <isuriharischandra2000@gmail.com>
2025-04-09 00:05:11 +0530

    Added DoctorsList.jsx

--- DocSmart/admin/src/pages/Admin/DoctorsList.jsx
+++ DocSmart/admin/src/pages/Admin/DoctorsList.jsx
@@ -9,11 +9,11 @@ const DoctorsList = () => {
     if (aToken) {
         getAllDoctors()
     }
-}, [aToken])
+  }, [aToken])
 
   return (
     <div className='m-5 max-h-[90vh] overflow-y-scroll'>
-      <h1 className='text-lg font-medium'>All Doctors</h1>
+      <h1 className='text-lg font-medium'>List of All Registered Doctors</h1>
       <div className='w-full flex flex-wrap gap-4 pt-5 gap-y-6'>
         {doctors.map((item, index) => (
           <div className='border border-[#C9D8FF] rounded-xl max-w-56 overflow-hidden cursor-pointer group' key={index}>
@@ -22,7 +22,7 @@ const DoctorsList = () => {
               <p className='text-[#262626] text-lg font-medium'>{item.name}</p>
               <p className='text-[#5C5C5C] text-sm'>{item.speciality}</p>
               <div className='mt-2 flex items-center gap-1 text-sm'>
-                <input onChange={()=>changeAvailability(item._id)} type="checkbox" checked={item.available} />
+                <input onChange={() => changeAvailability(item._id)} type="checkbox" checked={item.available} />
                 <p>Available</p>
               </div>
             </div>
